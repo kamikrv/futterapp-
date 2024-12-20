4905015ef6c5c6b55244ca55d763c9b756ddf602
kamikrv <115212496+kamikrv@users.noreply.github.com>
2024-12-20 14:28:55 +0500

    Update house_description.dart

--- lib/pages/house_description.dart
+++ lib/pages/house_description.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:github_app/local_storage/hive_storage.dart';
 import 'package:github_app/models/house.dart';
 
-
 class HomeDescription extends StatefulWidget {
   final House house;
   const HomeDescription({Key? key, required this.house}) : super(key: key);
@@ -18,7 +17,7 @@ class HomeDescriptionState extends State<HomeDescription> {
 
   Future init() async {
     final housesHive = await HiveStorage.loadHouses();
-    for(final house in housesHive) {
+    for (final house in housesHive) {
       if (house.firebaseId == widget.house.firebaseId) {
         houseHive = house;
         setState(() {});
@@ -29,121 +28,126 @@ class HomeDescriptionState extends State<HomeDescription> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: const Text('Roof.kz'),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                  houseHive == null ? Icons.star_border_outlined :
-                  Icons.star
-              ),
-              onPressed: () async {
-                if (houseHive == null) {
-                  houseHive = await HiveStorage.saveHouse(widget.house);
-                }else{
-                  await houseHive?.delete();
-                  houseHive = null;
-                }
-                setState(() {});
-                },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Roof.kz'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              houseHive == null ? Icons.star_border_outlined : Icons.star,
             ),
-            IconButton(
-              icon: const Icon(Icons.delete
-              ),
-              onPressed: () async {
-                showDialog(context: context,
-                    builder: (BuildContext context) =>
-                        CupertinoAlertDialog(
-                          title: const Text('Alert'),
-                          content: const Text(
-                              'Do you want to delete the ad?'),
-                          actions: <Widget>[
-                            CupertinoDialogAction(
-                              isDefaultAction: true,
-                              child: const Text("NO"),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            CupertinoDialogAction(
-                              isDefaultAction: true,
-                              child: const Text("Yes"),
-                              onPressed: () {
-                                try {
-                                  FirebaseFirestore.instance.collection(
-                                      'add_new').doc(
-                                      widget.house.firebaseId).delete();
-                                  Navigator.pop(context);
-                                  Navigator.pop(context);
-                                  // });
-                                } catch (error) {
-                                  print("Firebase error: ${error
-                                      .toString()}");
-                                }
-                              },
-                            ),
-                          ],
-                        )
-                );
-              },
-            )
-          ]),
-            body: FutureBuilder(
-                       future: init(),
-                        builder: (context, snapshot) {
-                         return Column(
-                         children: [
-                           Image(
-                              image: NetworkImage(widget.house.url),
-                              width: 450,
-                              height: 300,
-                              fit: BoxFit.fill,
-                             ),
-                           const SizedBox(
-                            width: 100,
-                            height: 40,
+            onPressed: () async {
+              if (houseHive == null) {
+                houseHive = await HiveStorage.saveHouse(widget.house);
+              } else {
+                await houseHive?.delete();
+                houseHive = null;
+              }
+              setState(() {});
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () async {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => CupertinoAlertDialog(
+                  title: const Text('Alert'),
+                  content: const Text('Do you want to delete the ad?'),
+                  actions: <Widget>[
+                    CupertinoDialogAction(
+                      isDefaultAction: true,
+                      child: const Text("NO"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    CupertinoDialogAction(
+                      isDefaultAction: true,
+                      child: const Text("Yes"),
+                      onPressed: () {
+                        try {
+                          FirebaseFirestore.instance
+                              .collection('add_new')
+                              .doc(widget.house.firebaseId)
+                              .delete();
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        } catch (error) {
+                          print("Firebase error: ${error.toString()}");
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: init(),
+        builder: (context, snapshot) {
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(
+                  image: NetworkImage(widget.house.url),
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Price:  ${widget.house.price}',
+                        style: const TextStyle(
+                          height: 2,
+                          fontSize: 20,
+                          color: Colors.black45,
                         ),
-                          Padding(
-                          padding:const EdgeInsets.fromLTRB(8, 16, 8, 16),
-                          child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Price:  ${widget.house.price}',
-                              style: const TextStyle(
-                                  height: 2,
-                                  fontSize: 20,
-                                  color: Colors.black45),
-                            ),
-                            Text('Count of rooms:  ${widget.house.countRooms}',
-                              style: const TextStyle(
-                                  height: 2,
-                                  fontSize: 20,
-                                  color: Colors.black45),
-                            ),
-                            Text('City:  ${widget.house.city}',
-                              style: const TextStyle(
-                                  height: 2,
-                                  fontSize: 20,
-                                  color: Colors.black45),
-                            ),
-                            Text('Description:  ${widget.house.description}',
-                              style: const TextStyle(
-                                  height: 2,
-                                  fontSize: 20,
-                                  color: Colors.black45),
-                            ),
-                          ],
-                          ),
-                          )
-                      ]
-                  );
-                })
-            )
-        );
+                      ),
+                      Text(
+                        'Count of rooms:  ${widget.house.countRooms}',
+                        style: const TextStyle(
+                          height: 2,
+                          fontSize: 20,
+                          color: Colors.black45,
+                        ),
+                      ),
+                      Text(
+                        'City:  ${widget.house.city}',
+                        style: const TextStyle(
+                          height: 2,
+                          fontSize: 20,
+                          color: Colors.black45,
+                        ),
+                      ),
+                      Text(
+                        'Description:  ${widget.house.description}',
+                        style: const TextStyle(
+                          height: 2,
+                          fontSize: 20,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
